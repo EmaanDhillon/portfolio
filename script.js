ac9c40594f939cc6d89f8346a45492df3f22888b
emaandhillon <emaaan.dhillon@gmail.com>
2025-09-15 02:29:35 +0530

    second

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Portfolio Loaded ✅");
+function showSection(sectionId) {
+  // Hide all sections
+  const sections = document.querySelectorAll('.section');
+  sections.forEach(sec => sec.style.display = "none");
+
+  // Show the clicked section
+  document.getElementById(sectionId).style.display = "block";
+}
